260a4944b6a0df7d7afc6b7cc8c3a977ebc7fd3a
Zangdarr <alexandre.verkyndt@gmail.com>
2015-11-08 16:56:43 +0100

    animeted_graph_tools.py : ajout de la gestion des nombres réelles. Affichage du front pareto UF1.

--- tools_folder/animated_graph_tools.py
+++ tools_folder/animated_graph_tools.py
@@ -2,12 +2,15 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 from decomposition_tools import genRatio_fctbase2
+import random
+import math
 
 title = "Default title"
 axe_x_name = "Default x axe"
 axe_y_name = "Default y axe"
 
 def update(data):
+    global tf1, tf2
     #clean graph
     plt.clf()
     #globals informations
@@ -15,7 +18,7 @@ def update(data):
     plt.xlabel(axe_x_name)
     plt.ylabel(axe_y_name)
     #recuparation data separetly
-    datax1y1, datax2y2, tic, evals, max_f1, max_f2, pop_size = data
+    datax1y1, datax2y2, tic, evals, max_f1, max_f2, pop_size, isReals = data
 
     #front pareto position in objective space
     x,y = datax1y1
@@ -26,7 +29,21 @@ def update(data):
     nb_functions = len(datax2y2)
 
     #draw axis
-    mm = max(max_f1, max_f2)+ 10
+    mm = 0
+    text_pos_x = 0
+    text_pos_y = 0
+    if(isReals):
+       max_f1 = int(max_f1)+1
+       max_f2 = int(max_f2)+1
+       mm = max(max_f1, max_f2)+ 1
+       plt.plot(tf1, tf2, color="purple", alpha=0.3)
+       text_pos_x = 1
+       text_pos_y = 1
+    else:
+       mm = max(max_f1, max_f2)+ 10
+       text_pos_x = 5
+       text_pos_y = 5
+
     zeros = np.array([0 for i in range(mm)])
     array = np.array([i for i in range(mm)])
     plt.plot(array,zeros, color='green')
@@ -55,20 +72,27 @@ def update(data):
     txt.append(" functions\n")
     txt.append("nb evaluations : ")
     txt.append(str(evals))
-    plt.text(5, 5, ''.join(txt))
+    plt.text(text_pos_x, text_pos_y, ''.join(txt))
 
     #draw z-optimal point and limit line
-    plt.scatter(max_f1, max_f2, color="yellow")
-    plt.plot([i for i in range(max_f1)], [max_f2 for i in range(max_f1)],color="yellow")
-    plt.plot([max_f1 for i in range(max_f2)], [i for i in range(max_f2)],color="yellow")
+    if(not(isReals)):
+        plt.scatter(max_f1, max_f2, color="yellow")
+        plt.plot([i for i in range(max_f1)], [max_f2 for i in range(max_f1)],color="yellow")
+        plt.plot([max_f1 for i in range(max_f2)], [i for i in range(max_f2)],color="yellow")
 
     #draw legend of the graph
     plt.legend()
     return 1
 
-
+tf1 = []
+tf2 = None
 def runAnimatedGraph(fct, titre, name_x, name_y, sleep=10):
-    global title, axe_x_name, axe_y_name
+    global title, axe_x_name, axe_y_name, tf1, tf2
+    tmp = 0.001
+    for i in range(1000):
+        tf1.append(tmp)
+        tmp += 0.001
+    tf2 = [1 - math.sqrt(tf1[i]) for i in range(1000)]
     title = titre
     axe_x_name = name_x
     axe_y_name = name_y
