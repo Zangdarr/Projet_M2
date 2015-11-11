c05abd6557d50acee1aee0047023e04528180d95
Zangdarr <alexandre.verkyndt@gmail.com>
2015-11-11 11:50:12 +0100

    decomposition_tools.py : suppression de la première version de la fonction d'obtention des ratios.

--- tools_folder/decomposition_tools.py
+++ tools_folder/decomposition_tools.py
@@ -1,27 +1,4 @@
 #-----------------------------------------------------------------------------------------------------------------------------------------------#
-"""
-NOT USED IN TCHEBY
-# generate N balanced new functions from the Nb base functions, return N + Nb functions
-def genDecompositionFunction(N_start_fct, N_new_fct, check=True):
-
-   ratio = genRatio_fctbase2(N_new_fct, check=True)
-
-   f1_numerateur = ratio[0]
-   f2_numerateur = ratio[1]
-   denumerateur  = ratio[2]
-
-   list_new_fct = []
-
-   for i in range(N_new_fct + N_start_fct):
-      new_fct = lambda f1, f2, i=i : (f1_numerateur[i]/denumerateur[i]) * f1 + (f2_numerateur[i]/denumerateur[i]) * f2
-      list_new_fct.append(new_fct)
-
-
-   if(check):
-     print("# genDecompositionFunction return fct list :\n", list_new_fct)
-
-   return list_new_fct
-"""
 #generate the ratio that will be used on both base functions for the N "new" functions, return a table [2][N + 2]
 def genRatio_fctbase2(N, check=False):
 
