7bd02f881cf775a1ac34e836b4a86f02ac3e4c07
Zangdarr <alexandre.verkyndt@gmail.com>
2015-11-29 21:53:10 +0100

    Ajout d'un main réalisant une expérimentation paramétrable de l'algorithme codé dans svm_moead_tools.py.

---
+++ programs_folder/main_run_predict.py
@@ -0,0 +1,16 @@
+import version5_reals_prediction as v5
+import sys
+
+
+problem_size               = 5
+training_neighborhood_size = 21
+nb_samples                 = 10
+strategy                  = 'all'
+
+
+#TODO sauvegarder les R² à chaques générations
+#TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les données utilisées pour l'apprentissage sont la population courante.S Une version plus avancée serait de prendre pour chaque direction les k dernières solutions qui ont minimisé la directions.
+
+
+nb_iterations = problem_size*50
+v5.runOneTime(problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy)
