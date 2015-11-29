1668b4d4904cbc794ba7e3b73e6b62cd4aa041be
Zangdarr <alexandre.verkyndt@gmail.com>
2015-11-29 20:59:26 +0100

    version4_reals_tcheby : gros commit, bug git, impossible de splitter les modifs, lire le commit en détail pour voir l'ensemble des modifs
    - MAJ du nom des fonctions d'évaluation
    - récuprération du titre du problème pour le plot
    - MAJ vecteur_size renommé en problem_size
    - récupération de l'espace de recherche du problème
    - le fichier version4 n'est plus un main
    - création de deux fonctions
    - fonction1 : lance l'algo avec un visuel graphique en utilisant les paramètres par défault du fichier version4 + les params passés en paramètre à la fonction
    - fonction2 : lance l'algo sans visuel graphique et écrit dans un fichier passé en paramètre les résultats toutes les 10 itérations.

--- programs_folder/version4_tcheby_real.py
+++ programs_folder/version4_tcheby_real.py
@@ -8,21 +8,20 @@ sys.path.insert(0,"../tools_folder/")
 import reals_tchebytchev_tools as tcheby
 
 #-----PARAM-----------------
-start_fct    = [fct.uf1_f1, fct.uf1_f2]
+start_fct    = [fct.f1, fct.f2]
 operator_fct = [op.DE_Operator, op.polynomial_mutation, op.repair_offspring]
 generation_fct = op.genVector
 start_fct_len = len(start_fct)
-
+pareto_front_fct = fct.getFrontPareto
+problem_title = fct.getProblemTitle()
 #Shall it maintain an archive and return it in the result ?
 manage_archive = False
 #stop critera : number of iterations
 nb_iterations = 100
 #data parameter : size of the input
-vector_size = 30
+problem_size = 10
 #size of the neighbourhood, include the current pos
 neighboring_size = 21
-#no need
-nb_flips = 1
 #number of new funtions to be generated
 N_new_fct = 98 #population size -2
 #slow down process parameter
@@ -35,17 +34,30 @@ delta_neighbourhood = 0.9
 CR = 1.0
 F = 0.5
 #proba to do the mutation, evaluate for each item in a single vector
-pm = 1 / vector_size
+#pm = 1 / problem_size
 distrib_index_n = 20
 
 
-search_space = fct.getSearchSpace(vector_size)
+search_space = fct.getSearchSpace(problem_size)
 
 #####################################################################################################################
 
 #number of functions that will be used for the algorithm
 nb_functions = start_fct_len + N_new_fct
 
-result = tcheby.getFrontPareto(start_fct, operator_fct, generation_fct, nb_functions, nb_iterations, neighboring_size, vector_size, nb_flips, max_decisions_maj, delta_neighbourhood, CR, search_space, F, distrib_index_n, pm, manage_archive, sleeptime)
+#result = tcheby.getFrontPareto(start_fct, operator_fct, generation_fct, nb_functions, nb_iterations, neighboring_size, problem_size, nb_flips, max_decisions_maj, delta_neighbourhood, CR, search_space, F, distrib_index_n, pm, manage_archive, sleeptime)
 
 #---------------------------------------------------------------------------------------------------------------------
+
+def runOneTime(problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy):
+       global problem_title, start_fct, operator_fct, generation_fct, pareto_front_fct, nb_functions, neighboring_size, max_decisions_maj, delta_neighbourhood, CR, F, distrib_index_n, pm, manage_archive, sleeptime
+       pm = 1 / problem_size
+       search_space = fct.getSearchSpace(problem_size)
+       tcheby.getFrontParetoWithGraphic(problem_title, start_fct, operator_fct, generation_fct, pareto_front_fct, nb_functions, nb_iterations, neighboring_size, problem_size, max_decisions_maj, delta_neighbourhood, CR, search_space, F, distrib_index_n, pm, manage_archive, -1, sleeptime)
+
+
+def experimentWith(file_to_write, problem_size,  nb_iterations):
+       global start_fct, operator_fct, generation_fct, nb_functions, neighboring_size, max_decisions_maj, delta_neighbourhood, CR, F, distrib_index_n, pm, manage_archive, sleeptime
+       pm = 1 / problem_size
+       search_space = fct.getSearchSpace(problem_size)
+       tcheby.getFrontParetoWithoutGraphic(start_fct, operator_fct, generation_fct, nb_functions, nb_iterations, neighboring_size, problem_size, max_decisions_maj, delta_neighbourhood, CR, search_space, F, distrib_index_n, pm, manage_archive, file_to_write, sleeptime)
