b9ce0f288746f2835891bec68d4867329bf54b32
Zangdarr <alexandre.verkyndt@gmail.com>
2015-11-29 21:46:17 +0100

    Ajout d'un fichier main exécutant reals_tcheby avec un affichage graphique sans stockage des résultats.

---
+++ programs_folder/main_run_no_predict.py
@@ -0,0 +1,17 @@
+import version4_tcheby_real as v4
+import sys
+
+
+problem_size               = 10
+training_neighborhood_size = 21
+nb_samples                 = 10
+strategy                  = 'single'
+
+
+#TODO sauvegarder les R² à chaques générations
+#TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les données utilisées pour l'apprentissage sont la population courante.S Une version plus avancée serait de prendre pour chaque direction les k dernières solutions qui ont minimisé la directions.
+
+
+nb_iterations = problem_size*50
+nb_iterations = 20000
+v4.runOneTime(problem_size, nb_iterations)
