c8b079ac1c460a1a5650c54f88c236cc91a09c8c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-12-05 15:38:26 +0100

    contient les fonctions servant au filtrage des offsprings générés à l'aide de modèles de prédiction.

---
+++ prediction_folder/filtring_tools.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+#--------------------------------------------------------------------------------------------------------------
+# MODEL-BASED-FILTRING
+
+
+def model_based_filtring(model, f_neighbors, list_offspring, model_directions):
+    id_offspring = -1
+    index_best = -1
+    score_best = 0
+    for offspring in list_offspring:
+        id_offspring += 1
+        average_score = 0
+
+        f_input_data = getInputData(f_neighbors, model_directions, offspring)
+
+        count = 0
+        for data in f_input_data:
+            tmp = model.predict(data)
+            average_score += tmp
+            count +=1
+
+        average_score /= count
+        if(index_best == -1):
+            index_best = id_offspring
+            score_best = average_score
+        elif(average_score < score_best):
+            index_best = id_offspring
+            score_best = average_score
+        else :
+            pass
+
+    return list_offspring[index_best]
+
+
+
+def getInputData(f_neighbors, model_directions, offspring):
+    l= []
+    id_l = -1
+    for i in f_neighbors:
+        id_l += 1
+        l.append([])
+
+        l[id_l].extend(model_directions[i].tolist()[0])
+
+        l[id_l].extend(offspring)
+
+    return np.matrix(l)
