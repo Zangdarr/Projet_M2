ef7b4173bd33b1e828117077d689050dbd46d30c
Zangdarr <alexandre.verkyndt@gmail.com>
2015-12-05 16:42:12 +0100

    Ajout du filtrage free eval.
    Ce filtrage n'utilise pas de model et applique des évaluations sans les comptés dans le budget.

--- prediction_folder/filtring_tools.py
+++ prediction_folder/filtring_tools.py
@@ -8,6 +8,9 @@ def model_based_filtring(filter_strat, param):
     if(filter_strat == 'average'):
         model, f_neighbors, list_offspring, model_directions = param
         return average_model_based(model, f_neighbors, list_offspring, model_directions)
+    elif(filter_strat == 'free'):
+        model, f_neighbors, list_offspring, model_directions, start_fct, problem_size, z_star = param
+        return free_model_based(model, f_neighbors, list_offspring, model_directions, start_fct, problem_size, z_star)
 
 
 def average_model_based(model, f_neighbors, list_offspring, model_directions):
@@ -39,6 +42,37 @@ def average_model_based(model, f_neighbors, list_offspring, model_directions):
 
     return list_offspring[index_best]
 
+
+def free_model_based(model, f_neighbors, list_offspring, model_directions, start_fct, problem_size, z_star):
+    id_offspring = -1
+    index_best = -1
+    score_best = 0
+    for offspring in list_offspring:
+        id_offspring += 1
+        average_score = 0
+
+        f_input_data = getInputData(f_neighbors, model_directions, offspring)
+
+        count = 0
+        for data in f_input_data:
+            w = data[0:2]
+            offs = data[2:]
+            score_eval = evals_to.free_eval(start_fct, offs, problem_size)
+            score_gtcheby = g_tcheby(w, score_eval, z_star)
+            average_score += score_gtcheby
+            count +=1
+
+        average_score /= count
+        if(index_best == -1):
+            index_best = id_offspring
+            score_best = average_score
+        elif(average_score < score_best):
+            index_best = id_offspring
+            score_best = average_score
+        else :
+            pass
+
+    return list_offspring[index_best]
 
 
 def getInputData(f_neighbors, model_directions, offspring):
