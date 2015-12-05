0588c3da919d65edf5f9f2358a8e15b06a341f2f
Zangdarr <alexandre.verkyndt@gmail.com>
2015-12-05 15:23:55 +0100

    suppression du code de la fonction d'initialisation random.

--- tools_folder/reals_tchebytchev_tools.py
+++ tools_folder/reals_tchebytchev_tools.py
@@ -23,16 +23,6 @@ def getResult():
     else:
         tmp = approx_pareto_front
     return tmp
-#--------------------------------------------------------------------------------------------------------------
-#INITIALISATION
-
-#Randomly initialise the best solution for each functions generated
-def initRandom(generation_fct, nb_functions, problem_size, search_space):
-    tab = []
-    for i in range(nb_functions):
-        tmp = generation_fct(problem_size, search_space)
-        tab.append(tmp)
-    return tab
 
 #--------------------------------------------------------------------------------------------------------------
 #  Sampling
