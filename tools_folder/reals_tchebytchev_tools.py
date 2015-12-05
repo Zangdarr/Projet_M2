60b6e85d6fbd9549eca8835928424b23eda36172
Zangdarr <alexandre.verkyndt@gmail.com>
2015-12-05 14:00:39 +0100

    Suppression des fonctions d'évaluations présentes.

--- tools_folder/reals_tchebytchev_tools.py
+++ tools_folder/reals_tchebytchev_tools.py
@@ -50,20 +50,6 @@ def initRandom(generation_fct, nb_functions, problem_size, search_space):
         tab.append(tmp)
     return tab
 
-#--------------------------------------------------------------------------------------------------------------
-#EVALUATION
-
-#evaluate the distance of a solution from the z-optimal solution
-def g_tcheby(dir, score, opt_scores):
-    return max(dir[0]*abs(score[0]-opt_scores[0]),dir[1]*abs(score[1]-opt_scores[1]))
-
-#evaluation of an offspring with the 2 base functions
-nb_evals = 0
-def eval(start_fct, problem, problem_size):
-    global nb_evals
-    nb_evals += 1
-    problem_scores = [start_fct[f](problem, problem_size) for f in range(len(start_fct))]
-    return problem_scores
 
 def maintain_population(decision_space, objective_space, pop_size):
     OS_0 = objective_space[0]
