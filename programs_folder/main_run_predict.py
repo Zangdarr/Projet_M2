b35bad3afff13e1a1c074fa1f8a45e94e336a3f6
Zangdarr <alexandre.verkyndt@gmail.com>
2015-12-05 17:21:43 +0100

    ajout du paramètre free_eval et filter_strat

--- programs_folder/main_run_predict.py
+++ programs_folder/main_run_predict.py
@@ -2,15 +2,16 @@ import version5_reals_prediction as v5
 import sys
 
 
-problem_size               = 5
+problem_size               = 16
 training_neighborhood_size = 21
 nb_samples                 = 10
-strategy                  = 'all'
-
+strategy                   = 'all'
+filter_strat               = 'average'
+free_eval                  = False
 
 #TODO sauvegarder les R² à chaques générations
 #TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les données utilisées pour l'apprentissage sont la population courante.S Une version plus avancée serait de prendre pour chaque direction les k dernières solutions qui ont minimisé la directions.
 
 
 nb_iterations = problem_size*50
-v5.runOneTime(problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy)
+v5.runOneTime(problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy, filter_strat, free_eval)
