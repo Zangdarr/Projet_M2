0d1b0f01581a3fb3fd968effaa306121b9296746
Zangdarr <alexandre.verkyndt@gmail.com>
2016-01-16 17:23:57 +0100

    Modification de la fonction printObjectives pour prendre en compte print_every.

--- tools_folder/reals_tchebytchev_tools.py
+++ tools_folder/reals_tchebytchev_tools.py
@@ -182,7 +182,7 @@ def runTcheby():
 
         #if write the result in a file
         if(writeOK):
-            printObjectives(file_to_write, nb_evals, itera+1, best_decisions_scores, problem_size)
+            printObjectives(file_to_write, nb_evals, itera+1, best_decisions_scores, problem_size, print_every=param_print_every)
             continue
 
         #graphic update
@@ -190,8 +190,11 @@ def runTcheby():
 
     return 1
 
-def printObjectives(file_to_write, eval_number,iteration_number,  objectives_table, problem_size):
-    if(iteration_number % problem_size == 0):
+def printObjectives(file_to_write, eval_number,iteration_number,  objectives_table, problem_size, print_every=-1):
+    modulo = problem_size
+    if(print_every != -1):
+        modulo = print_every
+    if(iteration_number % modulo == 0):
         tab = [''," ", '', " ", '', " ", '', "\n"]
         for objectives in objectives_table:
             tab[0] = str(iteration_number)
