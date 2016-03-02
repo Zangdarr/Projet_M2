2d696a4dbcc3070ffd98f02d4080ac930e869bd5
Zangdarr <alexandre.verkyndt@gmail.com>
2016-03-02 06:26:32 +0100

    paramétrage

--- programs_folder/experiment_no_predict.py
+++ programs_folder/experiment_no_predict.py
@@ -3,7 +3,7 @@ import sys
 
 
 #problem_size=[5,10, 30]
-problem_sizes               = [10]
+problem_sizes               = [10, 30]
 k = 20
 training_neighborhood_sizes = [21]
 
@@ -17,10 +17,11 @@ training_neighborhood_sizes = [21]
 for problem_size in problem_sizes:
 
     nb_iterations = problem_size*50
+    nb_iterations = 40
 
     for run in range(k):
 
-        filename = "UF4_MOEAD_PS-" + str(problem_size) + "_R-"+ str(run) +".txt"
+        filename = "UF3_MOEAD_PS-" + str(problem_size) + "_R-"+ str(run) +".txt"
         file_to_write = open(filename, 'a')
         v4.experimentWith(file_to_write ,problem_size, nb_iterations, 1)
         file_to_write.close()
