ba0bc3e10cf266d1a801672986ed327f6d88911a
Zangdarr <alexandre.verkyndt@gmail.com>
2016-03-02 06:36:49 +0100

    Modification de la boucle de lancement des algos.
    Ajout d'une boucle pour les stratégies.

--- programs_folder/experiment.py
+++ programs_folder/experiment.py
@@ -11,7 +11,7 @@ training_neighborhood_sizes = [21]
 #k = 30
 nb_sampless                 = [2, 4, 8, 16]
 strategies                  = ['single']
-filter_strat = "average"
+filter_strats = ["average", "maxdiff"]
 free_eval = [False, True]
 
 UF_name = "UF1"
@@ -26,31 +26,33 @@ nb_iterations = 40
 
 for run in range(k):
 
-    for problem_size in problem_sizes:
-        #nb_iterations = problem_size*50
+    for filter_strat in filter_strats:
 
-        for nb_samples in nb_sampless:
+        for problem_size in problem_sizes:
+            #nb_iterations = problem_size*50
 
-          for fe in free_eval:
+            for nb_samples in nb_sampless:
 
-            for strategy in strategies:
-                 if(strategy == 'neighbors'):
-                    for training_neighborhood_size in training_neighborhood_sizes:
-                        filename = UF_name + algo_name +"_PS-" + str(problem_size) + "_S-" + strategy + "_L-" + str(nb_samples) + "_TS-"+ str(training_neighborhood_size) + "_FS-" + filter_strat + "_FE-" + str(fe) + "_R-"+ str(run) +".txt"
-                        filenameR2 = "R2_" + filename
-                        file_to_write = open(filename, 'a')
-                        file_to_writeR2 = open(filenameR2, 'a')
-                        v5.experimentWith(file_to_write ,problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy, filter_strat, fe, param_print_every, file_to_writeR2)
-                        file_to_write.close()
-                        file_to_writeR2.close()
-                        print(filename, "done.")
-                 else:
-                        filename = UF_name + algo_name +"_PS-" + str(problem_size) + "_S-" + strategy + "_L-" + str(nb_samples) + "_TS--1" + "_FS-" + filter_strat + "_FE-" + str(fe) +"_R-"+ str(run) +".txt"
-                        filenameR2 = "R2_" + filename
-                        file_to_write = open(filename, 'a')
-                        file_to_writeR2 = open(filenameR2, 'a')
+              for fe in free_eval:
 
-                        v5.experimentWith(file_to_write, problem_size, nb_samples, nb_iterations, -1, strategy, filter_strat, fe, param_print_every, file_to_writeR2)
-                        file_to_write.close()
-                        file_to_writeR2.close()
-                        print(filename, "done.")
+                for strategy in strategies:
+                     if(strategy == 'neighbors'):
+                        for training_neighborhood_size in training_neighborhood_sizes:
+                            filename = UF_name + algo_name +"_PS-" + str(problem_size) + "_S-" + strategy + "_L-" + str(nb_samples) + "_TS-"+ str(training_neighborhood_size) + "_FS-" + filter_strat + "_FE-" + str(fe) + "_R-"+ str(run) +".txt"
+                            filenameR2 = "R2_" + filename
+                            file_to_write = open(filename, 'a')
+                            file_to_writeR2 = open(filenameR2, 'a')
+                            v5.experimentWith(file_to_write ,problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy, filter_strat, fe, param_print_every, file_to_writeR2)
+                            file_to_write.close()
+                            file_to_writeR2.close()
+                            print(filename, "done.")
+                     else:
+                            filename = UF_name + algo_name +"_PS-" + str(problem_size) + "_S-" + strategy + "_L-" + str(nb_samples) + "_TS--1" + "_FS-" + filter_strat + "_FE-" + str(fe) +"_R-"+ str(run) +".txt"
+                            filenameR2 = "R2_" + filename
+                            file_to_write = open(filename, 'a')
+                            file_to_writeR2 = open(filenameR2, 'a')
+
+                            v5.experimentWith(file_to_write, problem_size, nb_samples, nb_iterations, -1, strategy, filter_strat, fe, param_print_every, file_to_writeR2)
+                            file_to_write.close()
+                            file_to_writeR2.close()
+                            print(filename, "done.")
