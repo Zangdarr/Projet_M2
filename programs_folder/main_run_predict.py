77f5b9546f8b54c87702de0d33d8bec496867894
Zangdarr <alexandre.verkyndt@gmail.com>
2016-03-02 06:23:58 +0100

    paramétrage

--- programs_folder/main_run_predict.py
+++ programs_folder/main_run_predict.py
@@ -2,7 +2,7 @@ import version5_reals_prediction as v5
 import sys
 
 
-problem_size               = 16
+problem_size               = 10
 training_neighborhood_size = 21
 nb_samples                 = 10
 strategy                   = 'all'
@@ -13,5 +13,7 @@ free_eval                  = False
 #TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les données utilisées pour l'apprentissage sont la population courante.S Une version plus avancée serait de prendre pour chaque direction les k dernières solutions qui ont minimisé la directions.
 
 
-nb_iterations = problem_size*50
+#nb_iterations = problem_size*50
+nb_iterations = 40
+
 v5.runOneTime(problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy, filter_strat, free_eval)
