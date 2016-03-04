b2c103bda4e6aa6b6f60b2731a246e5e67b6c8e7
alexandre <alexandre.verkyndt@gmail.com>
2016-03-04 10:32:24 +0900

    commentaries UF8 to ASCII

--- programs_folder/main_run_no_predict.py
+++ programs_folder/main_run_no_predict.py
@@ -5,8 +5,8 @@ import sys
 problem_size               = 30
 
 
-#TODO sauvegarder les R^2 à chaques generations
-#TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les donnees utilisees pour l'apprentissage sont la population courante. S Une version plus avancee serait de prendre pour chaque direction les k dernières solutions qui ont minimise la directions.
+#TODO sauvegarder les R^2 a chaques generations
+#TODO faire un autre programme avec 2 modeles, 1 pour f1 et l'autre pour f2. Les donnees utilisees pour l'apprentissage sont la population courante. S Une version plus avancee serait de prendre pour chaque direction les k dernieres solutions qui ont minimise la directions.
 
 
 nb_iterations = problem_size*50
