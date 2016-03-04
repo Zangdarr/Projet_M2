f30cd5d1e3b2ea63a904688a75e7778d8c6c3fdb
alexandre <alexandre.verkyndt@gmail.com>
2016-03-04 14:10:31 +0900

    removing of useless comments

--- programs_folder/experiment_no_predict.py
+++ programs_folder/experiment_no_predict.py
@@ -10,9 +10,6 @@ training_neighborhood_sizes = [21]
 #problem_sizes = [5,10, 30]
 #k = 30
 
-#TODO sauvegarder les R² à chaques générations
-#TODO faire un autre programme avec 2 modèles, 1 pour f1 et l'autre pour f2. Les données utilisées pour l'apprentissage sont la population courante.S Une version plus avancée serait de prendre pour chaque direction les k dernières solutions qui ont minimisé la directions.
-
 
 for problem_size in problem_sizes:
 
