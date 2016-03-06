d8a06db6ce710a1827d2964ac188a052f1c95b9e
alexandre <alexandre.verkyndt@gmail.com>
2016-03-06 14:04:58 +0900

    UF1 python2 division compatible

--- functions_folder/UF1_functions.py
+++ functions_folder/UF1_functions.py
@@ -34,11 +34,11 @@ def f1(x_vector, vector_size):
    tmp_1 = x_vector[0]
 
    nb_odd = (vector_size-1) // 2
-   tmp_2 = 2 / nb_odd
+   tmp_2 = 2.0 / nb_odd
 
    tmp_3 = 0
    sin_tmp_1 = 6 * math.pi * tmp_1
-   pi_n = math.pi / vector_size
+   pi_n = math.pi / float(vector_size)
 
    for odd in range(2,vector_size, 2):
        sin_tmp_2 = odd+decalage * pi_n
@@ -54,11 +54,11 @@ def f2(x_vector, vector_size):
     tmp_1 = 1 - math.sqrt(x_1)
 
     nb_even = (vector_size-1) // 2 + ((vector_size-1)%2)
-    tmp_2 = 2 / nb_even
+    tmp_2 = 2.0 / nb_even
 
     tmp_3 = 0
     sin_tmp_1 = 6 * math.pi * x_1
-    pi_n = math.pi / vector_size
+    pi_n = math.pi / float(vector_size)
     for even in range(1,vector_size, 2):
         sin_tmp_2 = (even+decalage) * pi_n
         tmp_3 += x_vector[even] - math.sin(sin_tmp_1 + sin_tmp_2)
