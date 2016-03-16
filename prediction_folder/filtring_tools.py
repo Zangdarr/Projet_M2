a68b9e4b67a2cb5ec8bf6dbb6f4e3c6f0e96e817
alexandre <alexandre.verkyndt@gmail.com>
2016-03-16 10:09:42 +0900

    Update of numberdir function for the new results files.

--- prediction_folder/filtring_tools.py
+++ prediction_folder/filtring_tools.py
@@ -28,7 +28,7 @@ def model_based_filtring(filter_strat, free_eval,  param):
 #Return a candidat randomly selected within those that improve the maximum of direction within the current direction neighborhood
 def numberdir_score(free_eval, param):
 
-    current_f, model, model2, two_models_bool, f_neighbors, list_offspring, model_directions, start_fct, problem_size, z_star, population_scores = param
+    current_g, current_f, model, model2, two_models_bool, f_neighbors, list_offspring, model_directions, start_fct, problem_size, z_star, population_scores = param
 
 
     id_offspring = -1
@@ -40,23 +40,25 @@ def numberdir_score(free_eval, param):
 
         f_input_data = getInputData(f_neighbors, model_directions, offspring)
         if(not free_eval):
-           f_input_data = np.matrix(f_input_data)
-
-        f = 0
-        for data in f_input_data:
-            if(free_eval):
+           f_input_data_pred = np.matrix(f_input_data)
+           f = 0
+           for data in f_input_data_pred:
+               tmp = predict_and_quality(model, f_input_data[f], data, start_fct, problem_size, current_g, f_neighbors[f])
+               current_gtcheby = eval_to.g_tcheby(model_directions[f_neighbors[f]].tolist()[0], population_scores[f_neighbors[f]], z_star)
+               if(current_gtcheby > tmp):
+                   numberdir_score += 1
+               f +=1
+        else:
+            f = 0
+            for data in f_input_data:
                 w = data[0:2]
                 offs = data[2:]
                 score_eval = eval_to.free_eval(start_fct, offs, problem_size)
                 tmp = eval_to.g_tcheby(w, score_eval, z_star)
                 current_gtcheby = eval_to.g_tcheby(w , population_scores[f_neighbors[f]], z_star)
-            else:
-                tmp = model.predict(data)
-                current_gtcheby = eval_to.g_tcheby(model_directions[f_neighbors[f]].tolist()[0], population_scores[f_neighbors[f]], z_star)
-
-            if(current_gtcheby > tmp):
-                numberdir_score += 1
-            f +=1
+                if(current_gtcheby > tmp):
+                    numberdir_score += 1
+                f +=1
 
         offspring_result.append(numberdir_score)
 
