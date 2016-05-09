10ed820b7dc0aa9ae176950b85807b8e3bf78666
alexandre <alexandre.verkyndt@gmail.com>
2016-05-09 11:53:14 +0900

    comments

--- programs_folder/main_run_predict.py
+++ programs_folder/main_run_predict.py
@@ -2,16 +2,17 @@ import version5_reals_prediction as v5
 import sys
 
 
-problem_size               = 10
-training_neighborhood_size = 21
-nb_samples                 = 10
-strategy                   = 'all'
-filter_strat               = 'average'
-problem = "UF1"
-algo_name = "NuSVR_freeeval"
+problem_size               = 10          # 10 30
+training_neighborhood_size = 21          # 21
+nb_samples                 = 10          # 2 4 8 16
+strategy                   = 'all'       # single all
+filter_strat               = 'numberdir' # AvScl best by_direction numberdir
+problem = "UF1"                          # UF1 UF2 UF3 UF4 UF5 UF6 UF7 UF8 UF9 UF10
+algo_name = "NuSVR-pop"                  # NuSVR-freeeval NuSVR-pop NuSVR-popnewest NuSVR-newest NuSVR-fulleval
 
 
 #nb_iterations = problem_size*50
 nb_iterations = 40
 
+# launch the real-time graphical version of the algorithm with the above parameters
 v5.runOneTime(algo_name, problem, problem_size, nb_samples, nb_iterations, training_neighborhood_size, strategy, filter_strat)
