49cf74b41a91b2a5e7c1d1dca409b5f35c6e9382
alexandre <alexandre.verkyndt@gmail.com>
2016-05-09 11:20:13 +0900

    cleaning

--- programs_folder/version4_tcheby_real.py
+++ programs_folder/version4_tcheby_real.py
@@ -2,29 +2,18 @@
 
 import sys
 sys.path.insert(0, "../functions_folder/")
-#import UF1_functions as fct
 import operators_functions as op
 sys.path.insert(0,"../tools_folder/")
 import reals_tchebytchev_tools as tcheby
 
 #-----PARAM-----------------
-#start_fct    = fct.getObjectives()
 number_of_functions = 100
 operator_fct = [op.DE_Operator, op.polynomial_mutation, op.repair_offspring]
 generation_fct = op.genVector
-#start_fct_len = len(start_fct)
-#pareto_front_fct = fct.getFrontPareto
-#problem_title = fct.getProblemTitle()
 #Shall it maintain an archive and return it in the result ?
 manage_archive = False
-#stop critera : number of iterations
-#nb_iterations = 100
-#data parameter : size of the input
-#problem_size = 10
 #size of the neighbourhood, include the current pos
 neighboring_size = 21
-#number of new funtions to be generated
-#N_new_fct = number_of_functions - start_fct_len #population size - nb objective function
 #slow down process parameter
 sleeptime = 0
 #number max of replacement for each offspring
@@ -34,19 +23,8 @@ delta_neighbourhood = 0.9
 #proba to do the crossover, evaluate for each item in a single vector
 CR = 1.0
 F = 0.5
-#proba to do the mutation, evaluate for each item in a single vector
-#pm = 1 / problem_size
+
 distrib_index_n = 20
-
-
-#search_space = fct.getSearchSpace(problem_size)
-
-#####################################################################################################################
-
-#number of functions that will be used for the algorithm
-#nb_functions = start_fct_len + N_new_fct
-
-#result = tcheby.getFrontPareto(start_fct, operator_fct, generation_fct, nb_functions, nb_iterations, neighboring_size, problem_size, nb_flips, max_decisions_maj, delta_neighbourhood, CR, search_space, F, distrib_index_n, pm, manage_archive, sleeptime)
 
 #---------------------------------------------------------------------------------------------------------------------
 
