284abac136def7300f9eb2f66ddf0cf37ac1e867
Zangdarr <alexandre.verkyndt@gmail.com>
2016-05-19 09:28:44 +0200

    cluster_no_predict.py : set the seed with the run number.
    This to have a fair comparaison between run of the same number for the differents configurations.

--- programs_folder/cluster_no_predict.py
+++ programs_folder/cluster_no_predict.py
@@ -1,8 +1,12 @@
 import sys
 import version4_tcheby_real as v4
+import random
+
 
 args = sys.argv
 _, problem, problem_size, run, nb_iterations, param_print_every, result_folder = args
+
+random.seed(run)
 
 filename = result_folder + "/" + problem + "_MOEAD_PS-" + problem_size + "_R-"+ run +".txt"
 file_to_write = open(filename, 'a')
