8e8e75a7d60c1e8411e29e99e58b053e072d43df
Zangdarr <alexandre.verkyndt@gmail.com>
2016-08-12 04:19:54 +0200

    io_tools.py : implementation of a function to manage the generation of quality measurement files for a multimodel approach.

--- tools_folder/io_tools.py
+++ tools_folder/io_tools.py
@@ -32,3 +32,22 @@ def printR2(file_to_write, eval_number, iteration_number,  R2, R2_cv, MSE_cv, MA
 
 
             file_to_write.write(''.join(tab))
+
+def multimodel_printR2(file_to_write_list, eval_number, iteration_number,  R2, R2_cv, MSE_cv, MAE_cv, MDAE_cv, RMSE_cv, problem_size, nb_objectives, print_every=-1):
+        modulo = problem_size
+        if(print_every != -1):
+            modulo = print_every
+        if(iteration_number % modulo == 0):
+            tab = [''," ", '', " ", '', " ", '', " ", '', " ", ''," ", ''," ", '', "\n"]
+            tab[0] = str(iteration_number)
+            tab[2] = str(eval_number)
+            for i in range(nb_objectives):
+                tab[4] = str(round(R2[i],5))
+                tab[6] = str(round(R2_cv[i], 5))
+                tab[8] = str(round(MSE_cv[i], 5))
+                tab[10] = str(round(MAE_cv[i], 5))
+                tab[12] = str(round(MDAE_cv[i], 5))
+                tab[14] = str(round(RMSE_cv[i], 5))
+
+
+                file_to_write_list[i].write(''.join(tab))
